eb1c003b1900f4236e13bbeffe94f4c770611663
Sowmiya-Git <sowmiyaec2015@gmail.com>
2023-08-10 14:33:40 +0530

    my clone

--- src/com/test/Gitday2.java
+++ src/com/test/Gitday2.java
@@ -2,7 +2,8 @@ package com.test;
 
 public class Gitday2 {
 	public static void main(String[] args) {
-		System.out.println("sowmiya");
+		System.out.println("priya");
+		System.out.println("sowmi");
 	}
 
 }
